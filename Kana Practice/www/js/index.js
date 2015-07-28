9b584be74071be3c254a8d89700d9dde6d8ef605
Sheldon Marsh <thesheldon@googlemail.com>
2015-07-28 08:24:05 +0100

    first working

--- Kana Practice/www/js/index.js
+++ Kana Practice/www/js/index.js
@@ -16,6 +16,308 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+
+var kanaMod = angular.module('kanaMod', ['ngRoute']);
+
+kanaMod.controller('MainController', function($scope, $route){
+    $scope.test = "An initial test";
+});
+
+kanaMod.controller('KanaQuestion', function($scope){
+    
+
+/*
+* 0: a-o, hiragana
+* 1: ka-ko, hiragana
+* etc
+*/
+
+    $scope.kana = [
+        {
+            character:"あ",
+            set:0,
+            romaji:"a"
+        },
+        {
+            character:"い",
+            set:0,
+            romaji:"i"
+        },
+        {
+            character:"う",
+            set:0,
+            romaji:"u"
+        },
+        {
+            character:"え",
+            set:0,
+            romaji:"e"
+        },
+        {
+            character:"お",
+            set:0,
+            romaji:"o"
+        },
+
+
+        {
+            character:"か",
+            set:1,
+            romaji:"ka"
+        },        
+        {
+            character:"き",
+            set:1,
+            romaji:"ki"
+        },
+        {
+            character:"く",
+            set:1,
+            romaji:"ku"
+        },
+        {
+            character:"け",
+            set:1,
+            romaji:"ke"
+        },
+        {
+            character:"こ",
+            set:1,
+            romaji:"ko"
+        },
+
+
+        {
+            character:"さ",
+            set:2,
+            romaji:"sa"
+        },
+        {
+            character:"し",
+            set:2,
+            romaji:"shi"
+        },
+        {
+            character:"す",
+            set:2,
+            romaji:"su"
+        },
+        {
+            character:"せ",
+            set:2,
+            romaji:"se"
+        },
+        {
+            character:"そ",
+            set:2,
+            romaji:"so"
+        },
+
+
+        {
+            character:"た",
+            set:3,
+            romaji:"ta"
+        },
+        {
+            character:"ち",
+            set:3,
+            romaji:"chi"
+        },
+        {
+            character:"つ",
+            set:3,
+            romaji:"tsu"
+        },
+        {
+            character:"て",
+            set:3,
+            romaji:"te"
+        },
+        {
+            character:"と",
+            set:3,
+            romaji:"to"
+        },
+ 
+
+        {
+            character:"な",
+            set:4,
+            romaji:"na"
+        },
+        {
+            character:"に",
+            set:4,
+            romaji:"ni"
+        },
+        {
+            character:"ぬ",
+            set:4,
+            romaji:"nu"
+        },
+        {
+            character:"ね",
+            set:4,
+            romaji:"ne"
+        },
+        {
+            character:"の",
+            set:4,
+            romaji:"no"
+        },
+
+
+        {
+            character:"は",
+            set:5,
+            romaji:"ha"
+        },
+        {
+            character:"ひ",
+            set:5,
+            romaji:"hi"
+        },
+        {
+            character:"ふ",
+            set:5,
+            romaji:"fu"
+        },
+        {
+            character:"へ",
+            set:5,
+            romaji:"he"
+        },
+        {
+            character:"ほ",
+            set:5,
+            romaji:"ho"
+        },
+
+
+
+        {
+            character:"ま",
+            set:6,
+            romaji:"ma"
+        },
+        {
+            character:"み",
+            set:6,
+            romaji:"mi"
+        },
+        {
+            character:"む",
+            set:6,
+            romaji:"mu"
+        },
+        {
+            character:"め",
+            set:6,
+            romaji:"me"
+        },
+        {
+            character:"も",
+            set:6,
+            romaji:"mo"
+        },
+
+
+        {
+            character:"ら",
+            set:7,
+            romaji:"ra"
+        },
+        {
+            character:"り",
+            set:7,
+            romaji:"ri"
+        },
+        {
+            character:"る",
+            set:7,
+            romaji:"ru"
+        },
+        {
+            character:"れ",
+            set:7,
+            romaji:"re"
+        },
+        {
+            character:"ろ",
+            set:7,
+            romaji:"ro"
+        },
+
+
+        {
+            character:"わ",
+            set:8,
+            romaji:"wa"
+        },
+        {
+            character:"を",
+            set:8,
+            romaji:"wo"
+        },
+
+
+        {
+            character:"や",
+            set:9,
+            romaji:"ya"
+        },
+        {
+            character:"ゆ",
+            set:9,
+            romaji:"yu"
+        },
+        {
+            character:"よ",
+            set:9,
+            romaji:"yo"
+        },
+
+        {
+            character:"ん",
+            set:10,
+            romaji:"n"
+        },
+    ]
+
+    $scope.options = [];
+
+    $scope.newKana = function() {
+
+        $scope.options[0] = $scope.kana[Math.floor((Math.random()*$scope.kana.length))].romaji;
+        $scope.options[1] = $scope.kana[Math.floor((Math.random()*$scope.kana.length))].romaji;
+        $scope.options[2] = $scope.kana[Math.floor((Math.random()*$scope.kana.length))].romaji;
+
+        $scope.correctOption = Math.floor((Math.random()*3));
+        $scope.correctKana = $scope.kana[Math.floor((Math.random()*$scope.kana.length))]
+
+        $scope.options[$scope.correctOption] = $scope.correctKana.romaji;
+        $scope.currentKana = $scope.correctKana.character;
+    }
+
+    $scope.response = function(opt) {
+        console.log('clicked' + opt);
+        if (opt == $scope.correctOption) {
+            console.log('correct');
+            $scope.newKana();
+        }    
+    }
+
+    $scope.newKana();
+
+});
+
+// kanaMod.config(['$routeProvider',
+//     function($routeProvider) {
+//         $routeProvider.when('/')
+//     }])
+
 var app = {
     // Application Constructor
     initialize: function() {
